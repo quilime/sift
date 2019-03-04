5c7b562ba4b92b7ffc4ff069cfad48dde7036cd8
Gabriel Dunne <gdunne@quilime.com>
2019-03-03 22:05:51 -0800

    Updates

--- build.rb
+++ build.rb
@@ -27,40 +27,5 @@ Dir.glob("sift/**/*") do |file|
       output.each { |element| f.puts(element) }
     end
 
-    # puts "#{date.first}"
-
-    # out_file = File.new("#{path}out.txt", "w")
-    # out_file.puts("write your stuff here")
-
   end
 end
-
-# site.static_files.each do |file|
-#   if file.path["/sift/"]
-#     date = "#{File.mtime(file.path)}".split(' ')
-
-#     output = [
-#       "---",
-#       "title: #{file.basename}",
-#       "image_basename: #{File.basename(file.path)}",
-#       "image_path: #{file.path.split(Dir.pwd)[1]}",
-#       "categories: i",
-#       "layout: image",
-#       "---",
-#       "{{ page.image_path }}"
-#     ]
-
-#     filename = "#{path}/#{date.first}-#{file.basename}.html"
-#     File.open(filename, "w+") do |f|
-#       output.each { |element| f.puts(element) }
-#     end
-
-#     # puts "#{date.first}"
-
-#     # out_file = File.new("#{path}out.txt", "w")
-#     # out_file.puts("write your stuff here")
-    
-
-#       # site.pages << CategoryPage.new(site, site.source, "sift", file.path)
-#   end
-# end
